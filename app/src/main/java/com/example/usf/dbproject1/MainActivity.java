ea8997949ad06ccdf1d9be680288eb739628feae
Youssef El Khoury <usf.kh96@gmail.com>
2016-11-24 13:56:55 +0200

    This is a test push

--- app/src/main/java/com/example/usf/dbproject1/MainActivity.java
+++ app/src/main/java/com/example/usf/dbproject1/MainActivity.java
@@ -21,7 +21,7 @@ import com.example.usf.dbproject1.Fragments.SeriesFragment;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-
+    //this is a test push
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
