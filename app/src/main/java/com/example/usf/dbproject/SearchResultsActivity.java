cea4f63965ac4008cd3631926a5b83de4f83880f
Youssef El Khoury <usf.kh96@gmail.com>
2016-11-27 13:41:06 +0200

    Started working on the search feature

---
+++ app/src/main/java/com/example/usf/dbproject/SearchResultsActivity.java
@@ -0,0 +1,29 @@
+package com.example.usf.dbproject;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //use the query to search your data somehow
+        }
+    }
+
+}
